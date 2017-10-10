a30933905f02498cff1e1c571bb4afd25c8204be
vdoroshy <kjioyh9@gmail.com>
2017-10-10 19:02:47 +0300

    database.php quotation marks added

--- config/database.php
+++ config/database.php
@@ -2,7 +2,8 @@
 	$host = "127.0.0.1";
 	$dbname = "camagru";
 
-	$DB_DSN = 'mysql:dbname=$dbname;host=$host';
-	$DB_USER = root;
-	$DB_PASSWORD = 123456;
+	$DB_DSN = "mysql:dbname=$dbname;host=$host";
+	$DB_USER = "root";
+	$DB_PASSWORD = "123456";
+
 ?>
