1ec7b0a4f5bc7a662b8332f6268862f629224f32
vdoroshy <kjioyh9@gmail.com>
2017-10-17 17:51:03 +0300

    pdo is funbctioning: create and drop table and db

--- config/database.php
+++ config/database.php
@@ -3,7 +3,8 @@
 $host = "localhost";
 $dbname = "camagru";
 
-$DB_DSN = "mysql:dbname=$dbname;host=$host";
+$DB_DSN_CREATE = "mysql:host={$host}";
+$DB_DSN = "mysql:dbname={$dbname};host={$host}";
 $DB_USER = "root";
 $DB_PASSWORD = "123456";
 
--- config/setup.php
+++ config/setup.php
@@ -3,15 +3,28 @@
 include('database.php');
 
 try {
-	$pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-	if ($pdo) {
-		echo "connected";
-	}
-
+	$pdo = new PDO($DB_DSN_CREATE, $DB_USER, $DB_PASSWORD);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->exec("CREATE DATABASE IF NOT EXISTS `camagru`");
 
 } catch (PDOException $ex) {
-	echo "Connection failed: " . $ex->getMessage();//TODO
+	echo "Database was not created: " . $ex->getMessage();//TODO
 }
 
+try {
+	$pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//drop tables;
+	$pdo->exec("DROP TABLE IF EXISTS `users`");
+	$pdo->exec("CREATE TABLE IF NOT EXISTS `users`(
+		username CHAR(8) NOT NULL,
+		email VARCHAR(100) NOT NULL,
+		password VARCHAR(32) NOT NULL,
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY)
+		");
+
+} catch (PDOException $ex) {
+	echo "Connection failed: " . $ex->getMessage();
+}
 
 ?>
