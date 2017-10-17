c75204dac58b387b987f42e7a3a275935a047d7b
vdoroshy <kjioyh9@gmail.com>
2017-10-17 19:45:24 +0300

    trying things with  superglobal

--- registration.php
+++ registration.php
@@ -1,6 +1,16 @@
+<html>
+<body>
+
+
 <?php
 
 echo "successfully registered, not rly lal";
-
+echo "<br>";
+echo "user is " . $_GET['login'] . "<br>";
+echo "email is " . $_GET['email'] . "<br>";
+echo "password is " . $_GET['password'] . "<br>";
+echo "repeatPassword is " . $_GET['repeatPassword'] . "<br>";
 ?>
 
+</body>
+</html>
