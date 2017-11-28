35c4d77f3e6374ad58f6b7217cdbc4ff7d9c0638
Vladyslav Doroshyn <vdoroshy@e1r7p15.unit.ua>
2017-11-28 14:42:32 +0200

    deleted a not needed file

--- fileUpload.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-
-	echo $_FILES['file']['name'] ."<br/>";
-    echo $_FILES['file']['tmp_name']."<br/>";
-    echo $_FILES['file']['size']."<br/>";
-    echo $_FILES['file']['type']."<br/>";
-?>
