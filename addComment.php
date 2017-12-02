e8bb678c2920945c268c90529107f81c98327080
Vladyslav Doroshyn <vdoroshy@e1r7p15.unit.ua>
2017-12-02 17:03:31 +0200

    working on the comments, got a trouble with JOINs, the comments are now added to mysql and are shown via AJAX

--- addComment.php
+++ addComment.php
@@ -3,26 +3,49 @@ session_start();
 require_once('connectToDatabase.php');
 require('config/database.php');
 
-$loggedUser = $_SESSION['id'];
-
 $pdo = returnPDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 if ($pdo) {
 
-	// $stmt = $pdo->prepare("INSERT INTO `comments` (`photo_id`, `comment_text`, `commenter_id`, `owner_id`,)
-	// 						VALUES (:PhotoId, :CommentText, :CommenterId, :OwnerId)");
-	// $stmt->bindParam(':PhotoId', 7);
-	// $stmt->bindParam(':CommentText', "huy nana");
-	// $stmt->bindParam(':CommenterId', 1);
-	// $stmt->bindParam(':OwnerId', 1);
-	// $stmt->execute();
+	//sanitizing the comment
+	$comment = htmlentities($_POST['input']);
 
+	//getting the commeter id
+	$loggedUser = $_SESSION['id'];
+	$stmt = $pdo->prepare("SELECT * FROM `users` WHERE `username` = :LoggedUser");
+	$stmt->bindParam(':LoggedUser', $loggedUser);
+	$stmt->execute();
+	if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$commenterId = $row['id'];
+	}
+	//getting the photo id here
+	$path = $_POST['path'];
+	$stmt = $pdo->prepare("SELECT * FROM `photos` WHERE `photo_name` = :Name");
+	$stmt->bindParam(':Name', $path);
+	$stmt->execute();
+	if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$photoId = $row['id'];
+		$owner = $row['username'];
+	}
+	//getting the owner id
+	$stmt = $pdo->prepare("SELECT * FROM `users` WHERE `username` = :Owner");
+	$stmt->bindParam(':Owner', $owner);
+	$stmt->execute();
+	if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$ownerId = $row['id'];
+	}
 
-	// $success = json_encode($photos);
-	// echo $success ? $success : '';
-	echo "huy nana";
+	$stmt = $pdo->prepare("INSERT INTO `comments` (`photo_id`, `comment_text`, `commenter_id`, `owner_id`)
+							VALUES (:PhotoId, :CommentText, :CommenterId, :OwnerId)");
+	$stmt->bindParam(':PhotoId', $photoId);
+	$stmt->bindParam(':CommentText', $comment);
+	$stmt->bindParam(':CommenterId', $commenterId);
+	$stmt->bindParam(':OwnerId', $ownerId);
+	$stmt->execute();
+
+	echo "$loggedUser: $comment.";
 } else {
 	echo "no connection with the database<br/>";
 	die();
 }
-
+// JOINs could have solved it better, but my mysql structure has to be remodelled
 ?>
--- js/galleryPopup.js
+++ js/galleryPopup.js
@@ -43,11 +43,11 @@ function createPopup(event) {
   main.appendChild(btnDiv);
 
 
-  var comment = document.createElement('div');
+  var comments = document.createElement('div');
   var newComment = document.createElement('textarea')
   var btn = document.createElement('button');
 
-  comment.classList.add('gallery-popup-comments');
+  comments.classList.add('gallery-popup-comments');
   newComment.classList.add('gallery-popup-new-comment');
   btn.classList.add('gallery-popup-submit-comment');
   btn.addEventListener('click', addComment, true);
@@ -55,7 +55,7 @@ function createPopup(event) {
 
   newComment.name = "newComment";
   newComment.maxLength = "1000";
-  aside.appendChild(comment);
+  aside.appendChild(comments);
   aside.appendChild(newComment);
   aside.appendChild(btn);
 
@@ -79,9 +79,12 @@ function removeBlur(event) {
 }
 
 function addComment() {
-  var text = document.querySelector('.gallery-popup-new-comment');
+  var input = document.querySelector('.gallery-popup-new-comment');
+  //getting the url and cutting it to get the img name
+  var path = document.getElementsByTagName('img')[0].src;
+  var path = "userImages/" + path.split('/').pop();
 
-  if (text.value == "") {
+  if (input.value == "") {
     return;
   }
   var xhr = new XMLHttpRequest();
@@ -90,12 +93,15 @@ function addComment() {
       var comments = document.querySelector('.gallery-popup-comments');
 
       var elem = document.createElement('div');
+      elem.classList.add('gallery-popup-old-comment');
       elem.textContent = this.responseText;
+      //making the textarea blank
+      input.value = "";
       comments.appendChild(elem);
     }
   }
   xhr.open("POST", "addComment.php", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send();
+  xhr.send("input=" + input.value + "&path=" + path);
 }
 
