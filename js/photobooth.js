43f67398f5eab52b405973ab39225a76f6b0e711
vdoroshyn <kjioyh9@gmail.com>
2017-12-06 20:59:18 +0200

    console.log removed

--- js/photobooth.js
+++ js/photobooth.js
@@ -111,7 +111,6 @@ function saveImg() {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {        
-	    console.log(this.responseText);
 	    window.location.reload(true);
 	  }
 	}
